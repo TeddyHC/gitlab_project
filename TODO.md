069035f22d252ff5fa913d721096102b45bf2ed7
hongchao <teddy.hongchao@gmail.com>
2018-05-02 17:10:45 +0800

    add crontab for mac

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 # TODO:
 - conposer 找一个git操作库
 - 找一个v4接口，封装一个v4的接口库
-- mac 定时任务模版&& md
 - 记录切换前branch
 - 暂存代码的模块
 - 没有master的情况
 - 可以读到数据，但是not allowed
+- ssh config 配置和没配置情况兼容
---
+++ crontab/updateCode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+###############################################################
+# FileName: updateCode.sh
+# Author: Chao Hong <teddy.hongchao@gmail.com>
+# Program:
+###############################################################
+php /$yourPath/gitlab_project/bin/init.php > /dev/null
