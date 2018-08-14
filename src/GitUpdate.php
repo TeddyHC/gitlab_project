abdbd7ff8ab22d2089bdc74e7727df3899a1d4bf
hongchao <teddy.hongchao@gmail.com>
2018-08-14 12:22:21 +0800

    fix none ssh alias

--- src/GitUpdate.php
+++ src/GitUpdate.php
@@ -6,7 +6,7 @@ use gitUpdate\config\Config;
 use gitUpdate\config\MyConfig;
 
 /**
- * Class: gitUpdate
+ * Class: gitUpdate.
  *
  * @Author HongChao <hongchao@mafengwo.com>
  */
@@ -16,7 +16,7 @@ class GitUpdate
     private $config;
 
     /**
-     * __construct
+     * __construct.
      */
     public function __construct()
     {
@@ -26,7 +26,7 @@ class GitUpdate
 
     /**
      * _setConfig
-     * 读取配置文件,自己的配置写到\config\MyConfig,继承Config
+     * 读取配置文件,自己的配置写到\config\MyConfig,继承Config.
      */
     private function _setConfig()
     {
@@ -74,7 +74,7 @@ class GitUpdate
     }
 
     /**
-     * 获取所有项目
+     * 获取所有项目.
      */
     private function getAll()
     {
@@ -103,12 +103,12 @@ class GitUpdate
         foreach ($projects as $namespace => $repo) {
             var_dump($namespace);
             $gitStatus = exec('cd '.$this->config->storePath.'/'.$namespace.'; git status');
-            if ($gitStatus == 'nothing to commit, working tree clean'
-                || $gitStatus == 'no changes added to commit (use "git add" and/or "git commit -a")'
-                || $gitStatus == 'nothing added to commit but untracked files present (use "git add" to track)'
+            if ('nothing to commit, working tree clean' == $gitStatus
+                || 'no changes added to commit (use "git add" and/or "git commit -a")' == $gitStatus
+                || 'nothing added to commit but untracked files present (use "git add" to track)' == $gitStatus
             ) {
                 $result = shell_exec('cd '.$this->config->storePath.'/'.$namespace.'; git checkout master; git pull');
-                if ($result != 'Already up to date.') {
+                if ('Already up to date.' != $result) {
                     echo $result."\n";
                 }
             }
@@ -129,7 +129,7 @@ class GitUpdate
 
     /**
      * 检查有没有这个文件夹
-     * 没有先创建
+     * 没有先创建.
      */
     public function checkStock()
     {
@@ -139,7 +139,7 @@ class GitUpdate
     }
 
     /**
-     * 根据project接口返回值，拿到项目的vendor, projectName, ssh_url
+     * 根据project接口返回值，拿到项目的vendor, projectName, ssh_url.
      *
      * @param array $projects 接口返回的项目数组
      *
@@ -153,12 +153,16 @@ class GitUpdate
             if (in_array($project['path_with_namespace'], $ignores)) {
                 continue;
             }
+            $repo = $project['ssh_url_to_repo'];
+
             // 根据ssh config 替换url
-            $repo = str_replace(
-                'git@ssh.'.$this->config->baseUrl.':'.$this->config->sshPort,
-                $this->config->sshAlias,
-                $project['ssh_url_to_repo']
-            );
+            if ($this->config->sshAlias) {
+                $repo = str_replace(
+                    'git@ssh.'.$this->config->baseUrl.':'.$this->config->sshPort,
+                    $this->config->sshAlias,
+                    $project['ssh_url_to_repo']
+                );
+            }
             $projectNames[$project['path_with_namespace']] = $repo;
         }
 
@@ -166,15 +170,16 @@ class GitUpdate
     }
 
     /**
-     * 获取所有可见项目
+     * 获取所有可见项目.
      *
      * @param null $target
+     *
      * @return mixed
      */
     private function getProjects($target = null)
     {
         $url = $this->getUrl();
-        if($target) {
+        if ($target) {
             $this->addSearch($url, $target);
         }
         $projects = [];
@@ -195,7 +200,7 @@ class GitUpdate
     }
 
     /**
-     * Get Url
+     * Get Url.
      *
      * @return string
      */
@@ -211,12 +216,13 @@ class GitUpdate
 
     /**
      * 请求一页数据
-     * (为了不丢失数据，不提供pageSize参数, 保证每页请求条数一致)
+     * (为了不丢失数据，不提供pageSize参数, 保证每页请求条数一致).
      *
-     * @param string $url URL
-     * @param int $page 页号
+     * @param string $url  URL
+     * @param int    $page 页号
      *
      * @throws \GuzzleHttp\Exception\GuzzleException
+     *
      * @return mixed
      */
     private function _send($url, $page)
@@ -229,7 +235,7 @@ class GitUpdate
 
     /**
      * Url中加上在gitlab中生成的privateToken
-     * (类似身份认证)
+     * (类似身份认证).
      *
      * @param string $url URL
      */
@@ -239,7 +245,7 @@ class GitUpdate
     }
 
     /**
-     * addIsSimpleResponse
+     * addIsSimpleResponse.
      *
      * @param string $url      URL
      * @param string $isSimple 返回一个简略的结果
@@ -250,11 +256,11 @@ class GitUpdate
     }
 
     /**
-     * addPageInfo
+     * addPageInfo.
      *
      * @param mixed $url
-     * @param int $page
-     * @param int $pageSize
+     * @param int   $page
+     * @param int   $pageSize
      */
     private function addPageInfo(&$url, $page = 1, $pageSize = 100)
     {
@@ -262,7 +268,7 @@ class GitUpdate
     }
 
     /**
-     * _addOrderLimit
+     * _addOrderLimit.
      *
      * @param string $url     URL
      * @param string $orderBy 项目排序规则
@@ -274,7 +280,8 @@ class GitUpdate
     }
 
     /**
-     * Search for projects by name
+     * Search for projects by name.
+     *
      * @param $url
      * @param $content
      */
